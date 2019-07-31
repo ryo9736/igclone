0414cd5e05425c2abb7f205afa0e7b69e0725d2d
ryotaro yamashita <domdomgangan@gmail.com>
2019-07-31 14:03:36 +0900

    セキュアパスワード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   validates :email, presence: true,length:{ maximum: 40 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
+  has_secure_password
+  validates :password_digest, presence: true, length: { minimum: 6 }
 end
