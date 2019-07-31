bcc0fc223f374572ef70cd4d0711f41a2c0450d7
ryotaro yamashita <domdomgangan@gmail.com>
2019-07-31 11:46:15 +0900

    userモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
+  mount_uploader :image, ImageUploader
   has_many :posts
+  has_many :favorites,dependent:destroy
+  validates :name, presence: true,length: { maximum: 20 }
+  validates :email, presence: true,length:{ maximum: 40 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
