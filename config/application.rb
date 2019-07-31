065644faa501de1569d0bb3fe860b8f62e226b22
ryotaro yamashita <domdomgangan@gmail.com>
2019-07-31 17:34:12 +0900

    エラー修正

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,9 @@ module Igclone
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-
+    I18n.config.available_locales = :ja
     config.i18n.default_locale = :ja
-
     config.load_defaults 5.2
-
-
     config.generators do |g|
       # この二行の記述で自動生成しない設定を作成しています。
       g.assets false
