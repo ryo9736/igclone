4008f025fd7eb800f3f3b7d7f06d64d176636295
ryotaro yamashita <domdomgangan@gmail.com>
2019-07-31 13:29:26 +0900

    ユニーク制約作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
   validates :name, presence: true,length: { maximum: 20 }
   validates :email, presence: true,length:{ maximum: 40 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation { email.downcase! }
 end
