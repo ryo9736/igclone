f95dce36950580279ed2c18ce738b682a9543865
ryotaro yamashita <domdomgangan@gmail.com>
2019-07-31 15:19:57 +0900

    文法修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
   belongs_to :user
-  has_many :favorites,dependent:destroy
+  has_many :favorites,dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
   validates :title, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 80 }
