05550c4bce0ee5e7b245da5c4442be710bdf78be
ryotaro yamashita <domdomgangan@gmail.com>
2019-07-31 19:38:43 +0900

    エラー修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
- resources:sessions
- resources:favorites
+ resources:sessions,only: [:new, :create, :destroy]
+ resources:favorites,only: [:create, :destroy]
  resources:users
  resources:posts
 end
