f28dbdfa09bbcafb04a27fd5ee407b189528ea9d
ryotaro yamashita <domdomgangan@gmail.com>
2019-07-31 19:12:01 +0900

    エラーメッセージ追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,16 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user.id)
+    else
+      render 'new'
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
@@ -19,5 +26,11 @@ class UsersController < ApplicationController
   end
 
   def destrory
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :image, :image_cash)
   end
 end
