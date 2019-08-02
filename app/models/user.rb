666912502a287c10b2d1403cb26c0bdd0c6845c5
ryotaro yamashita <domdomgangan@gmail.com>
2019-08-02 16:50:06 +0900

    不要views 削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,13 @@
 class User < ApplicationRecord
-  mount_uploader :image, ImageUploader
   has_many :posts,dependent: :destroy
   has_many :favorites,dependent: :destroy
+  
   has_secure_password
   validates :password_digest, presence: true, length: { minimum: 6 }
   validates :name, presence: true,length: { maximum: 20 }
   validates :email, presence: true,length:{ maximum: 40 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
+
+  mount_uploader :image, ImageUploader
 end
