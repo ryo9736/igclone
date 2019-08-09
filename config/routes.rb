1b26eef068ba33d363975aeb202bb6b8416773f3
ryotaro yamashita <domdomgangan@gmail.com>
2019-08-09 11:40:50 +0900

    favorites index ルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources:sessions,only: [:new, :create, :destroy]
-  resources:favorites,only: [:create, :destroy]
+  resources:favorites,only: [:index,:create, :destroy]
   resources:users
   resources:posts do
     collection do
