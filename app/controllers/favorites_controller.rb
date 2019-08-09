99609ce702ae7f86aff62f67d29fb8e9c8dd9d49
ryotaro yamashita <domdomgangan@gmail.com>
2019-08-09 12:35:09 +0900

    kaminari実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,14 +2,13 @@ class FavoritesController < ApplicationController
   def index
     @user = current_user
     @post = @user.posts.all
-    @favorite = Favorite.where(user_id: @user.id).all
+    @favorites = Favorite.where(user_id: @user.id).all.page(params[:page])
   end
   def create
     @favorite = current_user.favorites.create(post_id: params[:post_id])
-    redirect_to posts_url, notice: "#{favorite.post.user.name}さんのブログをお気に入り登録しました"
-  end
+    redirect_to posts_url
   def destroy
     @favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to posts_url, notice: "#{favorite.post.user.name}さんのブログをお気に入り解除しました"
+    redirect_to posts_url
   end
 end
