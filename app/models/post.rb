83bdb01d4b11cb6a1eecc58fd26c937979599f71
ryotaro yamashita <domdomgangan@gmail.com>
2019-08-09 11:40:16 +0900

    gem kaminari追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
-  mount_uploader :image, ImageUploader
   belongs_to :user
   has_many :favorites,dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
   validates :title, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 80 }
   validates :image, presence: true
+
+  mount_uploader :image, ImageUploader
+  paginates_per 1
 end
