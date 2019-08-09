3328f72bdfd261bf896d4fac79e8e1335e843e68
ryotaro yamashita <domdomgangan@gmail.com>
2019-08-09 13:22:28 +0900

    エラー修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -7,6 +7,7 @@ class FavoritesController < ApplicationController
   def create
     @favorite = current_user.favorites.create(post_id: params[:post_id])
     redirect_to posts_url
+  end
   def destroy
     @favorite = current_user.favorites.find_by(id: params[:id]).destroy
     redirect_to posts_url
