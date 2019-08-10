ddc0a4db38af6ab81dd528fe81477d2a9efa1509
ryotaro yamashita <domdomgangan@gmail.com>
2019-08-10 19:22:25 +0900

    エラー修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only:[:show,:edit,:update,:destroy]
-  before_action :user_identify, only:[:show,:edit]
+  before_action :user_identify, only:[:edit]
 
   def index
     @users = User.all
