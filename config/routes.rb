05294a4b834a48676e9072f2bb01eeb1c62e837c
ryotaro yamashita <domdomgangan@gmail.com>
2019-08-10 17:38:26 +0900

    ルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
   resources:sessions,only: [:new, :create, :destroy]
   resources:favorites,only: [:index,:create, :destroy]
   resources:users
